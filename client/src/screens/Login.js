0765ef2dd2b0cc66395ca1d3a5703661cbe8427a
nly1020 <yingying.nly@gmail.com>
2024-10-16 22:59:57 +0800

    create-login-page

---
+++ client/src/screens/Login.js
@@ -0,0 +1,89 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import { CiUser } from "react-icons/ci";
+import { IoKeyOutline, IoEye, IoEyeOff } from "react-icons/io5";
+import Navbar from '../components/navbar/Navbar';
+import CustomButton from '../components/common/CustomButton';
+import TextField from '../components/common/TextField';
+
+const Login = () => {
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [visible, setVisible] = useState(false);
+    
+    const togglePasswordVisibility = () => {
+        setVisible((prevState) => !prevState);
+    }
+
+    return (
+        <div>
+            <Navbar />
+
+            <div className="flex justify-center items-center px-4 py-20">
+                <div className="w-full max-w-lg xl:px-8 xl:w-5/12 ml-auto mr-0">
+                    <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
+                        <h3 className="font-poppins mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
+                            Sign In
+                        </h3>
+                        <form className="mt-10">
+                            <TextField
+                                id={'username'}
+                                name={'username'}
+                                placeholder={'Username'}
+                                type={'text'}
+                                required
+                                icon={CiUser}
+                            />
+                            <div className="relative">
+                                <TextField
+                                    id={'password'}
+                                    name={'password'}
+                                    placeholder={'Password'}
+                                    type={visible ? 'text' : 'password'}
+                                    required
+                                    icon={IoKeyOutline}
+                                />
+                                <div
+                                    className="absolute inset-y-0 right-2 pr-3 flex items-center cursor-pointer"
+                                    onClick={togglePasswordVisibility}
+                                >
+                                    {visible ? (
+                                        <IoEye size={20} className="text-gray-400" />
+                                    ) : (
+                                        <IoEyeOff size={20} className="text-gray-400" />
+                                    )}
+                                </div>
+                            </div>
+
+                            <div className="font-poppins text-left">
+                                <Link
+                                    to='/forgot-password' 
+                                    className='text-sm hover:text-primary text-gray-500 underline transition duration-200'
+                                >
+                                    Forgot Password?
+                                </Link>
+                            </div>
+
+                            <div className="mt-12">
+                                <CustomButton title={'Login'} />
+                            </div>
+                        </form>
+
+                        {!isAdmin && (
+                            <div className="font-poppins text-center text-sm text-gray-500 mt-6">
+                                Hasn't have an account yet?{' '}
+                                <Link 
+                                    to='/register' 
+                                    className="underline hover:text-primary transition duration-200"
+                                >
+                                    Register here
+                                </Link>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
