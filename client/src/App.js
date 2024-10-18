cdeb9c7eeb7a1fe1e7f2c3cebdba4dec913876f5
nly1020 <yingying.nly@gmail.com>
2024-10-18 11:54:21 +0800

    Revert "ui-files-upload-button (#2)"
    This reverts commit 0a44a1f7d5f4e93daa907a0d41b39a2d337c47e7.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './screens/Home';
-import Login from './screens/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/common/FilesUploadButton.js
+++ client/src/components/common/FilesUploadButton.js
@@ -1,7 +1,5 @@
 import React, {useState} from 'react';
 import { PiUploadSimple } from "react-icons/pi";
-import { ImCancelCircle } from "react-icons/im";
-import { AiOutlineDelete } from "react-icons/ai";
 
 const FilesUploadButton = ({ className }) => {
     const [files, setFiles] = useState([]);
@@ -9,14 +7,14 @@ const FilesUploadButton = ({ className }) => {
     const handleFileChange = (e) => {
         const selectedFiles = Array.from(e.target.files);
         const validFiles = selectedFiles.filter(validateFile);
-        setFiles((prevFiles) => [...prevFiles, ...validFiles].slice(0, 3));
+        setFiles((prevFiles) => [...prevFiles, ...validFiles]);
     };
 
     const handleDrop = (e) => {
         e.preventDefault();
         const droppedFiles = Array.from(e.dataTransfer.files);
         const validFiles = droppedFiles.filter(validateFile);
-        setFiles((prevFiles) => [...prevFiles, ...validFiles].slice(0, 3));
+        setFiles((prevFiles) => [...prevFiles, ...validFiles]);
     };
 
     const validateFile = (file) => {
@@ -33,66 +31,40 @@ const FilesUploadButton = ({ className }) => {
         e.preventDefault();
     };
 
-    const handleDelete = (index) => {
-        setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
-    };
-
     return (
-        <>
-            <div className="flex flex-col items-center justify-center w-full">
-                <div className="border-2 border-dashed border-gray-300 rounded-lg px-6 py-6 text-center cursor-pointer w-full" onDrop={handleDrop} onDragOver={handleDragOver}>
-                    <div className="flex flex-col items-center justify-center">
-                        <label htmlFor="file-upload" className="flex flex-col items-center justify-center font-poppins text-gray-500 rounded-full w-16 h-16 bg-indigo-50 shadow-lg mt-2">
-                            <PiUploadSimple className="text-primary" size={32} />
-                        </label>
-                        <div className="font-poppins text-black mt-8">
-                            <label htmlFor="file-upload" className="underline font-semibold text-primary cursor-pointer mr-1">
-                                Click here
-                            </label>
-                            to upload your file or drag and drop
-                        </div>
-                        <input
-                            id="file-upload"
-                            type="file"
-                            accept=".svg,.jpg,.jpeg,.png"
-                            className="hidden"
-                            onChange={handleFileChange}
-                            multiple
-                        />
-                        <div className="font-poppins text-sm text-gray-500 mt-3">
-                            Supported Format: SVG, JPG, PNG
-                        </div>
-                    </div>
+        <div className={'border-2 border-dashed border-gray-300 rounded-lg px-6 py-6 text-center cursor-pointer w-full'} onDrop={handleDrop} onDragOver={handleDragOver}>
+            <div className="flex flex-col items-center justify-center">
+                <label htmlFor="file-upload" className="flex flex-col items-center justify-center font-poppins text-gray-500 rounded-full w-16 h-16 bg-indigo-50 shadow-lg">
+                    <PiUploadSimple className="text-primary" size={32}/>
+                </label>
+                <div className="font-poppins text-black mt-10">
+                    <label htmlFor="file-upload" className="underline font-semibold text-primary cursor-pointer mr-1">
+                        Click here
+                    </label> 
+                    to upload your file or drag and drop
                 </div>
-
+                <input
+                    id="file-upload"
+                    type="file"
+                    accept=".svg,.jpg,.jpeg,.png"
+                    className="hidden"
+                    onChange={handleFileChange}
+                    multiple
+                />
+                <div className="font-poppins text-sm text-gray-500 mt-3">
+                    Supported Format: SVG, JPG, PNG
+                </div>
                 {files.length > 0 && (
-                    <div className="mt-6 w-full">
-                        <div className={`grid gap-2 justify-items-center ${files.length === 2 ? 'grid-cols-2' : 'grid-cols-3'}`}>
-                            {files.map((file, index) => (
-                                <div key={index} className="relative w-full">
-                                    <div className="w-full h-36 bg-gray-100 overflow-hidden">
-                                        <img
-                                            src={URL.createObjectURL(file)}
-                                            alt={file.name}
-                                            className="w-full h-full object-cover"
-                                        />
-                                    </div>
-                                    <div className="flex items-center justify-center mt-2 mr-2 font-poppins text-sm break-words">
-                                        <button
-                                            onClick={() => handleDelete(index)}
-                                            className="rounded-full p-1 border border-gray-100 shadow-lg hover:bg-red-100 transition duration-200 mr-2"
-                                        >
-                                            <AiOutlineDelete className="text-red-500" size={20} />
-                                        </button>
-                                        <span>{file.name}</span>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
+                    <div className="mt-3 text-sm text-gray-700">
+                        <ul>
+                        {files.map((file, index) => (
+                            <li key={index}>{file.name}</li>
+                        ))}
+                        </ul>
                     </div>
                 )}
             </div>
-        </>
+        </div>
     );
 };
 
