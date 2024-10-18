0a44a1f7d5f4e93daa907a0d41b39a2d337c47e7
nly1020 <yingying.nly@gmail.com>
2024-10-18 11:53:14 +0800

    ui-files-upload-button (#2)
    1. Add image preview section

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
 
           <div className='hidden lg:flex lg:flex-1 lg:justify-end'>
             <Link
-              to='/'
+              to='/login'
               className='font-poppins font-semibold text-secondary hover:text-primary transition duration-200'
             >
               Login <span aria-hidden='true'>&rarr;</span>
