56d614eedf46f9810b0b6263691c625cafb09814
nly1020 <yingying.nly@gmail.com>
2024-10-27 14:31:09 +0800

    Card (#3)
    Change bg color

--- client/src/components/common/Card.js
+++ client/src/components/common/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ header, children }) => {
   return (
-    <div className={`bg-gray-100 rounded-lg p-6 shadow-md mt-6 mb-6 w-full mx-auto`}>
+    <div className={`bg-gray-50 rounded-lg p-6 shadow-md mt-6 mb-6 w-full mx-auto`}>
       {header && (
         <div className="font-poppins text-lg font-semibold mb-4">
           {header}
