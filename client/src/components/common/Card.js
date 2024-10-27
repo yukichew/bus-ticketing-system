0aaa0f8c8c033f7dc068655ea8ece3234003afed
nly1020 <yingying.nly@gmail.com>
2024-10-27 19:31:06 +0800

    Card (#4)

--- client/src/components/common/Card.js
+++ client/src/components/common/Card.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Card = ({ header, children }) => {
+const Card = ({ header, children, Icon, onClick, tooltip }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
   return (
-    <div className={`bg-gray-50 rounded-lg p-6 shadow-md mt-6 mb-6 w-full mx-auto`}>
+    <div className={`bg-gray-50 rounded-lg p-6 shadow-md mt-6 w-full mx-auto`}>
       {header && (
-        <div className="font-poppins text-lg font-semibold mb-4">
-          {header}
+        <div className="flex justify-between items-start">
+          <div className="font-poppins font-semibold text-lg text-primary mb-4">
+            {header}
+          </div>
+          {Icon && (
+            <div 
+              onClick={onClick} 
+              className="relative cursor-pointer"
+              onMouseEnter={() => setShowTooltip(true)} 
+              onMouseLeave={() => setShowTooltip(false)}
+            >
+              <Icon className="w-6 h-6 text-gray-500 hover:text-primary transition duration-200" />
+              {showTooltip && (
+                <div className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 bg-gray-700 text-white text-xs font-poppins rounded py-2 px-4">
+                  {tooltip}
+                </div>
+              )}
+            </div>
+          )}
         </div>
       )}
       <div className="space-y-4">
