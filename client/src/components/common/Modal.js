415f03c5feb98ace9654e55b8027dba9a6fcb350
yukichew <yukichew0803@gmail.com>
2024-10-29 02:51:39 +0800

    update modal box

---
+++ client/src/components/common/Modal.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { IoClose } from 'react-icons/io5';
+
+const Modal = ({ isVisible, onClose, children, className }) => {
+  if (!isVisible) return null;
+
+  return (
+    <div className='fixed inset-0 bg-black bg-opacity-55 flex justify-center items-center'>
+      <div className={`bg-white rounded-lg shadow-lg p-6 w-auto' ${className}`}>
+        <IoClose
+          size={25}
+          className='cursor-pointer float-right text-gray-400 hover:text-gray-800'
+          onClick={onClose}
+          aria-label='Close'
+        />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- client/src/components/user/Seatmap.js
+++ client/src/components/user/Seatmap.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { IoClose } from 'react-icons/io5';
 import { RiSteering2Line } from 'react-icons/ri';
 import CustomButton from '../common/CustomButton';
+import Modal from '../common/Modal'; // Import the Modal component
 import Seat from './Seat';
 
 const Seatmap = () => {
@@ -25,52 +25,41 @@ const Seatmap = () => {
     <>
       <CustomButton title='Select Seat' type='button' onClick={toggleModal} />
 
-      {showModal && (
-        <div className='fixed inset-0 bg-gray-900 bg-opacity-55 flex justify-center items-center'>
-          <div className='bg-white p-7 rounded-lg w-auto relative'>
-            <IoClose
-              size={25}
-              className='cursor-pointer absolute top-3 right-3 text-gray-400 hover:text-gray-800'
-              onClick={toggleModal}
-            />
-            <h2 className='text-lg lg:text-xl font-semibold font-poppins'>
-              Select your seat
-            </h2>
+      <Modal isVisible={showModal} onClose={toggleModal} className='w-auto'>
+        <h2 className='text-lg lg:text-xl font-semibold font-poppins'>
+          Select your seat
+        </h2>
 
-            <div className='flex justify-end items-center my-3 bg-gray-200 p-2 rounded'>
-              <RiSteering2Line size={25} className='mr-4 text-gray-500' />
-            </div>
-
-            <div
-              className='grid grid-cols-custom mb-4'
-              style={{
-                gridTemplateColumns: '1fr 2fr 1fr 1fr',
-                columnGap: '10px',
-              }}
-            >
-              {seats.map((seatNumber) => (
-                <div className='flex' key={seatNumber}>
-                  <Seat
-                    key={seatNumber}
-                    seatNumber={seatNumber}
-                    isSelected={selectedSeats.includes(seatNumber)}
-                    isOccupied={false}
-                    onSelect={handleSelect}
-                  />
-                </div>
-              ))}
-            </div>
-
-            <CustomButton
-              title='Proceed to Book'
-              type='button'
-              onClick={() => {
-                toggleModal();
-              }}
-            />
-          </div>
+        <div className='flex justify-end items-center my-3 bg-gray-200 p-2 rounded'>
+          <RiSteering2Line size={25} className='mr-4 text-gray-500' />
         </div>
-      )}
+
+        <div
+          className='grid grid-cols-custom mb-4'
+          style={{
+            gridTemplateColumns: '1fr 2fr 1fr 1fr',
+            columnGap: '10px',
+          }}
+        >
+          {seats.map((seatNumber) => (
+            <div className='flex' key={seatNumber}>
+              <Seat
+                key={seatNumber}
+                seatNumber={seatNumber}
+                isSelected={selectedSeats.includes(seatNumber)}
+                isOccupied={false}
+                onSelect={handleSelect}
+              />
+            </div>
+          ))}
+        </div>
+
+        <CustomButton
+          title='Proceed to Book'
+          type='button'
+          onClick={toggleModal}
+        />
+      </Modal>
     </>
   );
 };
