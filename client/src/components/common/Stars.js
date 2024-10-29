ae72e09f01abbfb6a0ac4f92519fa21546a90ca4
yukichew <yukichew0803@gmail.com>
2024-10-30 01:36:46 +0800

    completed rating and reviews page

--- client/src/components/common/Stars.js
+++ client/src/components/common/Stars.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { FaStar } from 'react-icons/fa6';
 
-const Stars = ({ rating }) => {
+const Stars = ({ rating, size = 18 }) => {
   const filledStars = Math.floor(rating);
   const emptyStars = 5 - filledStars;
 
   return (
-    <div className='flex justify-center space-x-1'>
+    <div className='flex space-x-1'>
       {[...Array(filledStars)].map((_, index) => (
-        <FaStar key={index} size={18} className='fill-yellow-400' />
+        <FaStar key={index} size={size} className='fill-yellow-400' />
       ))}
       {[...Array(emptyStars)].map((_, index) => (
-        <FaStar key={index + filledStars} size={18} className='fill-gray-300' />
+        <FaStar
+          key={index + filledStars}
+          size={size}
+          className='fill-gray-300'
+        />
       ))}
     </div>
   );
---
+++ client/src/components/user/RatingSummary.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FaStar } from 'react-icons/fa6';
+import Stars from '../common/Stars';
+
+const RatingBar = ({ rating, percentage }) => {
+  return (
+    <div className='flex items-center'>
+      <p className='text-sm text-gray-600 font-semibold w-8'>{rating}</p>
+      <FaStar size={18} className='fill-gray-600' />
+      <div className='bg-gray-300 rounded w-full h-3 ml-3'>
+        <div
+          className='h-full rounded bg-yellow-400'
+          style={{ width: `${percentage}%` }}
+        ></div>
+      </div>
+      <p className='text-sm text-gray-600 font-semibold ml-3 w-10'>
+        {percentage}%
+      </p>
+    </div>
+  );
+};
+
+const RatingSummary = () => {
+  return (
+    <div className='max-w-sm font-poppins'>
+      <h2 className='font-semibold text-lg'>Ratings</h2>
+
+      {/* overall ratings */}
+      <div className='text-center flex items-center mb-5'>
+        <Stars rating={4} />
+        <p className='pl-2 text-sm text-gray-600 font-semibold'>
+          4.0 <span className='text-xs'>(153 Ratings)</span>
+        </p>
+      </div>
+
+      {/* ratings overview */}
+      <div className='space-y-2'>
+        <RatingBar rating='5.0' percentage={66} />
+        <RatingBar rating='4.0' percentage={33} />
+        <RatingBar rating='3.0' percentage={16} />
+        <RatingBar rating='2.0' percentage={8} />
+        <RatingBar rating='1.0' percentage={6} />
+      </div>
+    </div>
+  );
+};
+
+export default RatingSummary;
