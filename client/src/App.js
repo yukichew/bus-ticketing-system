058efcdeacf6d8472ca310f4dd032acb1ff73306
yukichew <yukichew0803@gmail.com>
2024-10-29 14:26:35 +0800

    resolve confict

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './screens/user/Home';
 import UserProfile from './screens/busOperator/profile/UserProfile';
+import Home from './screens/user/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        {/* Auth */}
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/otp-verification' element={<OTPVerification />} />
-        <Route path='/create-account' element={<SignUp />} />
-        <Route path='/reset-password' element={<ResetPassword />} />
-
         {/* Bus Operator */}
         <Route path='/bo/user-profile' element={<UserProfile />} />
       </Routes>
