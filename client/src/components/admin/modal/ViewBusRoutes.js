a790bd38c39ef5d7881d0064d42961a17272b5fe
jezlynnn <jezlynwy@gmail.com>
2024-11-05 03:38:59 +0800

    Added filter and action items logic

--- client/src/components/admin/modal/ViewBusRoutes.js
+++ client/src/components/admin/modal/ViewBusRoutes.js
@@ -3,7 +3,7 @@ import CustomButton from "../../../components/common/CustomButton";
 import CustomInput from "../../../components/common/CustomInput";
 import Card from "../../../components/common/Card";
 
-const ViewBusRoutes = ({ operator, onClose }) => {
+const ViewBusRoutes = ({ operator, onClose, isApproved }) => {
   const [routeInfo, setRouteInfo] = useState({
     busPlate: "",
     origin: "",
@@ -33,99 +33,97 @@ const ViewBusRoutes = ({ operator, onClose }) => {
   };
 
   return (
-    <div className="flex flex-col space-y-4 w-full">
-      <Card header="Bus Route Details">
-        <div className="pointer-events-none">
-          <label
-            htmlFor="busPlate"
-            className="block text-sm font-poppins font-medium text-gray-700 pb-2"
-          >
-            Bus Plate
-          </label>
-          <CustomInput
-            placeholder="Enter bus plate"
-            id="busPlate"
-            name="busPlate"
-            type="text"
-            value={routeInfo.busPlate}
-            onChange={handleRouteChange}
-            required
-          />
-        </div>
-        <div className="pointer-events-none">
-          <label
-            htmlFor="origin"
-            className="block text-sm font-poppins font-medium text-gray-700 pb-2"
-          >
-            Origin
-          </label>
-          <CustomInput
-            placeholder="Enter origin"
-            id="origin"
-            name="origin"
-            type="text"
-            value={routeInfo.origin}
-            onChange={handleRouteChange}
-            className="pointer-events-none"
-            required
-          />
-        </div>
-        <div className="pointer-events-none">
-          <label
-            htmlFor="destination"
-            className="block text-sm font-poppins font-medium text-gray-700 pb-2"
-          >
-            Destination
-          </label>
-          <CustomInput
-            placeholder="Enter destination"
-            id="destination"
-            name="destination"
-            type="text"
-            value={routeInfo.destination}
-            onChange={handleRouteChange}
-            className="pointer-events-none"
-            required
-          />
-        </div>
-        <div className="pointer-events-none">
-          <label
-            htmlFor="etd"
-            className="block text-sm font-poppins font-medium text-gray-700 pb-2"
-          >
-            ETD
-          </label>
-          <CustomInput
-            placeholder="Enter ETD"
-            id="etd"
-            name="etd"
-            type="text"
-            value={routeInfo.etd}
-            onChange={handleRouteChange}
-            className="pointer-events-none"
-            required
-          />
-        </div>
-        <div className="pointer-events-none">
-          <label
-            htmlFor="eta"
-            className="block text-sm font-poppins font-medium text-gray-700 pb-2"
-          >
-            ETA
-          </label>
-          <CustomInput
-            placeholder="Enter ETA"
-            id="eta"
-            name="eta"
-            type="text"
-            value={routeInfo.eta}
-            onChange={handleRouteChange}
-            className="pointer-events-none"
-            required
-          />
-        </div>
-        <CustomButton title={"Approve Bus Routes"} />
-      </Card>
+    <div className="flex flex-col space-y-4 w-[400px]">
+      <div className="pointer-events-none">
+        <label
+          htmlFor="busPlate"
+          className="block text-sm font-poppins font-medium text-gray-700 pb-2"
+        >
+          Bus Plate
+        </label>
+        <CustomInput
+          placeholder="Enter bus plate"
+          id="busPlate"
+          name="busPlate"
+          type="text"
+          value={routeInfo.busPlate}
+          onChange={handleRouteChange}
+          required
+        />
+      </div>
+      <div className="pointer-events-none">
+        <label
+          htmlFor="origin"
+          className="block text-sm font-poppins font-medium text-gray-700 pb-2"
+        >
+          Origin
+        </label>
+        <CustomInput
+          placeholder="Enter origin"
+          id="origin"
+          name="origin"
+          type="text"
+          value={routeInfo.origin}
+          onChange={handleRouteChange}
+          className="pointer-events-none"
+          required
+        />
+      </div>
+      <div className="pointer-events-none">
+        <label
+          htmlFor="destination"
+          className="block text-sm font-poppins font-medium text-gray-700 pb-2"
+        >
+          Destination
+        </label>
+        <CustomInput
+          placeholder="Enter destination"
+          id="destination"
+          name="destination"
+          type="text"
+          value={routeInfo.destination}
+          onChange={handleRouteChange}
+          className="pointer-events-none"
+          required
+        />
+      </div>
+      <div className="pointer-events-none">
+        <label
+          htmlFor="etd"
+          className="block text-sm font-poppins font-medium text-gray-700 pb-2"
+        >
+          ETD
+        </label>
+        <CustomInput
+          placeholder="Enter ETD"
+          id="etd"
+          name="etd"
+          type="text"
+          value={routeInfo.etd}
+          onChange={handleRouteChange}
+          className="pointer-events-none"
+          required
+        />
+      </div>
+      <div className="pointer-events-none">
+        <label
+          htmlFor="eta"
+          className="block text-sm font-poppins font-medium text-gray-700 pb-2"
+        >
+          ETA
+        </label>
+        <CustomInput
+          placeholder="Enter ETA"
+          id="eta"
+          name="eta"
+          type="text"
+          value={routeInfo.eta}
+          onChange={handleRouteChange}
+          className="pointer-events-none"
+          required
+        />
+      </div>
+      {!isApproved && <CustomButton title={"Approve Bus Routes"} />}
     </div>
   );
 };
--- client/src/screens/admin/ManageBusRoutes.js
+++ client/src/screens/admin/ManageBusRoutes.js
@@ -4,131 +4,124 @@ import AdminHeader from "../../components/admin/AdminHeader";
 import Table from "../../components/common/Table";
 import { MdCancel } from "react-icons/md";
 import { IoEye } from "react-icons/io5";
+import { TiTick } from "react-icons/ti";
+import { IoFilter } from "react-icons/io5";
+import { busRoutes } from "../../components/constants/Dummy";
 import Modal from "../../components/common/Modal";
 import ViewBusRoutes from "../../components/admin/modal/ViewBusRoutes";
+import Status from "../../components/admin/Status";
+import Card from "../../components/common/Card";
+import CustomInput from "../../components/common/CustomInput";
 
 const ManageBusRoutes = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false); // state for approve modal
+  const [showDetailsModal, setShowDetailsModal] = useState(false); // separate state for details modal
   const [selectedOperator, setSelectedOperator] = useState(null);
+  const [showFilters, setShowFilters] = useState(false);
+  const [filters, setFilters] = useState({
+    busPlate: "",
+    origin: "",
+    destination: "",
+    etd: "",
+    eta: "",
+    status: "",
+  });
 
   const toggleSidebar = () => {
     setIsSidebarOpen((prev) => !prev);
   };
 
-  const [data, setData] = useState([
-    {
-      id: "1",
-      busPlate: "ABC 1234",
-      origin: "Kuala Lumpur",
-      destination: "Penang",
-      etd: "09:00",
-      eta: "11:00",
-    },
-    {
-      id: "2",
-      busPlate: "DEF 5678",
-      origin: "Johor Bahru",
-      destination: "Kuala Lumpur",
-      etd: "09:00",
-      eta: "11:00",
-    },
-    {
-      id: "3",
-      busPlate: "GHI 9101",
-      origin: "Malacca City",
-      destination: "Kota Kinabalu",
-      etd: "09:00",
-      eta: "11:00",
-    },
-    {
-      id: "4",
-      busPlate: "JKL 2345",
-      origin: "George Town",
-      destination: "Kuala Lumpur",
-      etd: "09:00",
-      eta: "11:00",
-    },
-    {
-      id: "5",
-      busPlate: "MNO 6789",
-      origin: "Kota Kinabalu",
-      destination: "Sandakan",
-      etd: "09:00",
-      eta: "11:00",
-    },
-    {
-      id: "6",
-      busPlate: "PQR 3456",
-      origin: "Ipoh",
-      destination: "Kuala Lumpur",
-      etd: "09:00",
-      eta: "11:00",
-    },
-    {
-      id: "7",
-      busPlate: "STU 7890",
-      origin: "Kuching",
-      destination: "Sibu",
-      etd: "09:00",
-      eta: "11:00",
-    },
-    {
-      id: "8",
-      busPlate: "VWX 1230",
-      origin: "Kuala Terengganu",
-      destination: "Kota Bharu",
-      etd: "09:00",
-      eta: "11:00",
-    },
-    {
-      id: "9",
-      busPlate: "YZA 5678",
-      origin: "Shah Alam",
-      destination: "Malacca City",
-      etd: "09:00",
-      eta: "11:00",
-    },
-    {
-      id: "10",
-      busPlate: "BCD 9012",
-      origin: "Seremban",
-      destination: "Kuala Lumpur",
-      etd: "09:00",
-      eta: "11:00",
-    },
-  ]);
+  const columns = [
+    "Bus Plate",
+    "Origin",
+    "Destination",
+    "ETD",
+    "ETA",
+    "Status",
+  ];
+  const columnKeys = [
+    "busPlate",
+    "origin",
+    "destination",
+    "etd",
+    "eta",
+    "status",
+  ];
 
-  const columns = ["Bus Plate", "Origin", "Destination", "ETD", "ETA"];
-  const columnKeys = ["busPlate", "origin", "destination", "etd", "eta"];
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: value,
+    }));
+  };
+
+  const filteredData = busRoutes.filter((operator) =>
+    Object.keys(filters).every((key) =>
+      operator[key].toLowerCase().includes(filters[key].toLowerCase())
+    )
+  );
+
+  const enhancedData = filteredData.map((item) => ({
+    ...item,
+    status: (
+      <div className="flex justify-center">
+        <Status status={item.status} />
+      </div>
+    ),
+    originalStatus: item.status,
+  }));
 
   const actionIcons = (row) => (
     <div className="flex justify-center space-x-2">
       {/* View Button */}
-      <div className="relative group">
-        <button
-          onClick={() => {
-            setShowModal(true);
-            setSelectedOperator(row);
-          }}
-          className="text-grey-500 hover:text-grey-600"
-        >
-          <IoEye className="text-xl" />
-        </button>
-        <span className="absolute left-1/2 transform -translate-x-1/2 -translate-y-8 bg-gray-700 text-white text-xs rounded p-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          View Details
-        </span>
-      </div>
+      {row.originalStatus !== "Approved" && (
+        <div className="relative group">
+          <button
+            onClick={() => {
+              setShowModal(true);
+              setSelectedOperator(row);
+            }}
+            className="text-green-500 hover:text-green-600"
+          >
+            <TiTick className="text-2xl" />
+          </button>
+          <span className="absolute left-1/2 transform -translate-x-1/2 -translate-y-8 bg-gray-700 text-white text-xs rounded p-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            Approve
+          </span>
+        </div>
+      )}
 
-      {/* reject button */}
-      <div className="relative group">
-        <button className="text-red-500 hover:text-red-600">
-          <MdCancel className="text-xl" />
-        </button>
-        <span className="absolute left-1/2 transform -translate-x-1/2 -translate-y-8 bg-gray-700 text-white text-xs rounded p-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          Reject
-        </span>
-      </div>
+      {/* Reject Button */}
+      {row.originalStatus !== "Approved" && (
+        <div className="relative group">
+          <button className="text-red-500 hover:text-red-600">
+            <MdCancel className="text-xl" />
+          </button>
+          <span className="absolute left-1/2 transform -translate-x-1/2 -translate-y-8 bg-gray-700 text-white text-xs rounded p-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            Reject
+          </span>
+        </div>
+      )}
+
+      {/* View Details Button */}
+      {row.originalStatus !== "Pending" && (
+        <div className="relative group">
+          <button
+            onClick={() => {
+              setShowDetailsModal(true); // Set the details modal to show
+              setSelectedOperator(row);
+            }}
+            className="text-grey-500 hover:text-grey-600"
+          >
+            <IoEye className="text-xl" />
+          </button>
+          <span className="absolute left-1/2 transform -translate-x-1/2 -translate-y-8 bg-gray-700 text-white text-xs rounded p-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            View Details
+          </span>
+        </div>
+      )}
     </div>
   );
 
@@ -151,9 +144,88 @@ const ManageBusRoutes = () => {
               Bus Routes Management
             </h2>
           </div>
+
+          {/* filter button */}
+          <button
+            className="ml-auto flex items-center font-medium hover:text-primary pr-1"
+            onClick={() => setShowFilters((prev) => !prev)} // Toggle filter visibility
+          >
+            <IoFilter size={16} />
+            <p className="mx-1">Filters</p>
+          </button>
+
+          {/* filter input */}
+          {showFilters && (
+            <Card>
+              <div className="flex justify-between gap-4">
+                <CustomInput
+                  placeholder="Filter by Bus Plate"
+                  id="busPlate"
+                  name="busPlate"
+                  type="text"
+                  value={filters.busPlate}
+                  onChange={handleFilterChange}
+                />
+                <CustomInput
+                  placeholder="Filter by Origin"
+                  id="origin"
+                  name="origin"
+                  type="text"
+                  value={filters.origin}
+                  onChange={handleFilterChange}
+                />
+                <CustomInput
+                  placeholder="Filter by Destination"
+                  id="destination"
+                  name="destination"
+                  type="text"
+                  value={filters.destination}
+                  onChange={handleFilterChange}
+                />
+                <CustomInput
+                  placeholder="Filter by ETD"
+                  id="etd"
+                  name="etd"
+                  type="text"
+                  value={filters.etd}
+                  onChange={handleFilterChange}
+                />
+                <CustomInput
+                  placeholder="Filter by ETA"
+                  id="eta"
+                  name="eta"
+                  type="text"
+                  value={filters.eta}
+                  onChange={handleFilterChange}
+                />
+                {/* Dropdown for Status */}
+                <select
+                  id="status"
+                  name="status"
+                  value={filters.status}
+                  onChange={handleFilterChange}
+                  className="w-full h-12 px-4 rounded ring-1 ring-gray-300 focus:ring-primary focus:outline-none font-poppins text-sm"
+                >
+                  <option value="">All Status</option>{" "}
+                  <option value="approved">Approved</option>
+                  <option value="pending">Pending</option>
+                </select>
+              </div>
+            </Card>
+          )}
+
+          <div className="flex justify-between items-center mt-8">
+            <p className="text-gray-500">
+              <span className="font-semibold text-secondary">
+                {filteredData.length} bus route applications
+              </span>{" "}
+              found
+            </p>
+          </div>
+
           <div className="mt-3 mx-auto">
             <Table
-              data={data}
+              data={enhancedData}
               columns={columns}
               columnKeys={columnKeys}
               showActionColumn={true}
@@ -161,15 +233,23 @@ const ManageBusRoutes = () => {
             />
           </div>
 
-          {/* modal for view details */}
-          <Modal
-            isVisible={showModal}
-            onClose={() => setShowModal(false)}
-            className="w-2/4"
-          >
+          {/* modal for approve */}
+          <Modal isVisible={showModal} onClose={() => setShowModal(false)}>
             <ViewBusRoutes
               operator={selectedOperator}
               onClose={() => setShowModal(false)}
+            />
+          </Modal>
+
+          {/* Modal for View Details */}
+          <Modal
+            isVisible={showDetailsModal}
+            onClose={() => setShowDetailsModal(false)}
+          >
+            <ViewBusRoutes
+              operator={selectedOperator}
+              onClose={() => setShowDetailsModal(false)}
+              isApproved={selectedOperator?.originalStatus === "Approved"}
             />
           </Modal>
         </div>
