4c724aa409eeb180775a1e8764749c71f3e6cb55
jezlynnn <jezlynwy@gmail.com>
2024-11-17 00:36:26 +0800

    Add in tab component to render section for user management page

--- client/src/constants/TabItems.jsx
+++ client/src/constants/TabItems.jsx
@@ -1,20 +1,27 @@
-import Carousel from '../components/common/Carousel';
-import Notes from '../screens/user/modal/Notes';
-import Ratings from '../screens/user/modal/Ratings';
-import BusTypes from '../components/busOperator/BusTypes';
-import BusScheduling from '../components/busOperator/BusScheduling';
-import { bus } from './Dummy';
+import Carousel from "../components/common/Carousel";
+import Notes from "../screens/user/modal/Notes";
+import Ratings from "../screens/user/modal/Ratings";
+import BusTypes from "../components/busOperator/BusTypes";
+import BusScheduling from "../components/busOperator/BusScheduling";
+import ManageBusOperator from "../components/admin/ManageBusOperators";
+import ManageUser from "../components/admin/ManageUser";
+import { bus } from "./Dummy";
 
 export const busInfoTabs = [
-  { label: 'Notes', content: <Notes /> },
-  { label: 'Photos', content: <Carousel images={bus.images} /> },
+  { label: "Notes", content: <Notes /> },
+  { label: "Photos", content: <Carousel images={bus.images} /> },
   {
-    label: 'Ratings & Reviews',
+    label: "Ratings & Reviews",
     content: <Ratings />,
   },
 ];
 
 export const busManagementTabs = [
-  { label: 'Bus Types', content: <BusTypes /> },
-  { label: 'Bus Scheduling', content: <BusScheduling /> },
+  { label: "Bus Types", content: <BusTypes /> },
+  { label: "Bus Scheduling", content: <BusScheduling /> },
+];
+
+export const manageUserTabs = [
+  { label: "Passengers", content: <ManageUser /> },
+  { label: "Bus Operators", content: <ManageBusOperator /> },
 ];
