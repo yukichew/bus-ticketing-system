5daa3256a828a195d10b474307352d8dcdb3aa53
yukichew <yukichew0803@gmail.com>
2024-12-02 04:05:45 +0800

    update user model with otp

--- server/Models/User.cs
+++ server/Models/User.cs
@@ -4,6 +4,9 @@ namespace server.Models
 {
     public class User : IdentityUser
     {
+        public string? EmailOTP { get; set; }
+        public DateTime? OTPExpiry { get; set; }
 
+        public DateTime? LastOTPSent { get; set; }
     }
 }
