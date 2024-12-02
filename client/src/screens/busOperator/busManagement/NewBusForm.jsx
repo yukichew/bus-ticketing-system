f351507155de3cc7a84b99d62dae2246f8d03098
nly1020 <yingying.nly@gmail.com>
2024-12-02 12:34:21 +0800

    Modify Bus Operator Screens
    1, Change the location of Bus Type and No. of Seats
    2. UserRegistration: Remove DOB input for passengers
    3. Modify Mange Bus Screens

---
+++ client/src/screens/busOperator/busManagement/NewBusForm.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from 'react';
+import { RiArrowDropDownLine } from "react-icons/ri";
+import Navbar from '../../../components/common/Navbar';
+import Footer from '../../../components/Footer';
+import Card from '../../../components/common/Card';
+import CustomButton from '../../../components/common/CustomButton';
+import CustomInput from '../../../components/common/CustomInput';
+
+const NewBusForm = () => {
+    const [isBusTypeOpen, setIsBusTypeOpen] = useState(false);
+    const [selectedBusTypeOption, setSelectedBusTypeOption] = useState('Select a bus type');
+    const [isBusStatusOpen, setIsBusStatusOpen] = useState(false);
+    const [selectedBusStatusOption, setSelectedBusStatusOption] = useState('Select a status');
+
+    const busTypeOptions = ['2+1', '2+2'];
+
+    const busStatusOptions = ['Active', 'Inactive'];
+
+    const handleSelectBusType = (option) => {
+        setSelectedBusTypeOption(option);
+        setIsBusTypeOpen(false);
+    };
+
+    const handleSelectBusStatus = (option) => {
+        setSelectedBusStatusOption(option);
+        setIsBusStatusOpen(false);
+    };
+
+    return(
+        <>
+            <Navbar />
+
+            <div className='w-4/5 mt-8 mx-auto'>
+                <div className='flex items-center'>
+                    <h2 className='font-poppins font-bold text-2xl'>Bus Management</h2>
+                </div>
+
+                <Card header="Bus Information">
+                    <div className="flex space-x-4 mt-2">
+                        <div className="w-1/2">
+                            <label htmlFor="busPlate" className="block text-sm font-poppins font-medium text-gray-700 mb-2">Bus Plate</label>
+                            <CustomInput
+                                type="text"
+                                id="busPlate"
+                                className="mt-2 block w-full text-sm font-poppins rounded-lg p-2 ring-1 ring-gray-300 focus:ring-primary focus:outline-none"
+                                placeholder="Enter Bus Plate"
+                            />
+                        </div>
+
+                        <div className="relative inline-block text-left flex-1">
+                            <label htmlFor="busType" className="block text-sm font-poppins font-medium text-gray-700">Bus Type</label>
+                            <button
+                                onClick={() => setIsBusTypeOpen(!isBusTypeOpen)}
+                                className={`inline-flex justify-between items-center w-full h-12 rounded-md border border-gray-300 shadow-sm px-4 mt-2 bg-white text-sm font-poppins font-small focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 ${selectedBusTypeOption === 'Select a bus type' ? 'text-gray-400' : 'text-black'}`}
+                            >
+                                {selectedBusTypeOption}
+                                <RiArrowDropDownLine className="ml-2 h-5 w-5" />
+                            </button>
+
+                            {isBusTypeOpen && (
+                                <div className="absolute z-10 mt-1 w-full rounded-md bg-white shadow-lg">
+                                    <ul className="max-h-56 rounded-md py-1 text-base font-poppins ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                                        {busTypeOptions.map((option, index) => (
+                                            <li
+                                                key={index}
+                                                onClick={() => handleSelectBusType(option)}
+                                                className="cursor-pointer select-none relative py-2 pl-3 pr-9 hover:bg-gray-100"
+                                            >
+                                                {option}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+
+                    <div className="flex space-x-4 mt-4">
+                        <div className="w-1/2">
+                            <label htmlFor="numSeats" className="block text-sm font-poppins font-medium text-gray-700 mb-2">No. of Seats</label>
+                            <CustomInput
+                                type="number"
+                                id="numSeats"
+                                min="1"
+                                className="mt-2 block w-full text-sm font-poppins rounded-lg p-2 ring-1 ring-gray-300 focus:ring-primary focus:outline-none"
+                                placeholder="Enter Number of Seats"
+                            />
+                        </div>
+
+                        <div className="relative inline-block text-left flex-1">
+                            <label htmlFor="busStatus" className="block text-sm font-poppins font-medium text-gray-700">Bus Status</label>
+                            <button
+                                onClick={() => setIsBusStatusOpen(!isBusStatusOpen)}
+                                className={`inline-flex justify-between items-center w-full h-12 rounded-md border border-gray-300 shadow-sm px-4 mt-2 bg-white text-sm font-poppins font-small focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 ${selectedBusStatusOption === 'Select a status' ? 'text-gray-400' : 'text-black'}`}
+                            >
+                                {selectedBusStatusOption}
+                                <RiArrowDropDownLine className="ml-2 h-5 w-5" />
+                            </button>
+
+                            {isBusStatusOpen && (
+                                <div className="absolute z-10 mt-1 w-full rounded-md bg-white shadow-lg">
+                                    <ul className="max-h-56 rounded-md py-1 text-base font-poppins ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                                        {busStatusOptions.map((option, index) => (
+                                            <li
+                                                key={index}
+                                                onClick={() => handleSelectBusStatus(option)}
+                                                className="cursor-pointer select-none relative py-2 pl-3 pr-9 hover:bg-gray-100"
+                                            >
+                                                {option}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </Card>
+                
+                <div className='mt-8 mb-10'>
+                    <CustomButton title='Create' className='font-semibold'/>
+                </div>
+            </div>
+
+            <Footer />
+        </>
+    );
+}
+
+export default NewBusForm;
