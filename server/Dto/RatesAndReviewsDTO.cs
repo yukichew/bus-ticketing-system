5dd719939fae65711982510fb9323b9ea3694706
nly1020 <yingying.nly@gmail.com>
2024-12-08 16:47:02 +0800

    Rates and reviews

--- server/Dto/RatesAndReviewsDTO.cs
+++ server/Dto/RatesAndReviewsDTO.cs
@@ -1,18 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
 namespace server.Dto
 {
     public class RatesAndReviewsDTO
     {
-        public int ID { get; set; }
-
         public string BusOperatorID { get; set; }
-
+        [Required(ErrorMessage = "Comment is required")]
         public string Comment { get; set; }
         [Required(ErrorMessage = "Rate is required")]
         public int Rate { get; set; }
         [Required(ErrorMessage = "PostedById is required")]
-        public int PostedById { get; set; }
+        public Guid PostedById { get; set; }
         [Required(ErrorMessage = "Status is required")]
         public string Status { get; set; }
     }
