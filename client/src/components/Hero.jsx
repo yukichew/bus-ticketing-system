00f9a8478fe44960f7793491a5cee1d8b1a691e0
Yuki Chew <115348525+yukichew@users.noreply.github.com>
2024-12-11 02:45:36 +0800

    Search bus validation (#108)

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
   };
 
   return (
-    <section className='relative'>
+    <section className='relative z-0'>
       <img
         src='https://images.unsplash.com/photo-1469474968028-56623f02e42e?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         className='absolute inset-0 object-cover w-full h-full'
--- client/src/constants/NavbarItems.js
+++ client/src/constants/NavbarItems.js
@@ -1,12 +1,13 @@
 export const userLinks = [
   { key: 1, name: 'Home', href: '/' },
-  { key: 2, name: 'About', href: '/about' },
+  { key: 2, name: 'Contact', href: '/contact-us' },
 ];
 
 export const busOperatorLinks = [
   { key: 1, name: 'Dashboard', href: '/bo/dashboard' },
   { key: 2, name: 'Bus Management', href: '/bo/bus' },
-  { key: 3, name: 'Rates & Review', href: '/bo/rates-and-reviews' },
+  { key: 3, name: 'Sales Revenue', href: '/bo/sales-revenue' },
+  { key: 4, name: 'Rates & Review', href: '/bo/rates-and-reviews' },
 ];
 
 export const memberLinks = [{ key: 2, name: 'Booking', href: '/trips' }];
