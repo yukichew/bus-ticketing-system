2fc896c7f139dbe1194a58e3a2d83d794f287e09
yukichew <yukichew0803@gmail.com>
2024-12-11 02:45:13 +0800

    solve hero and navbar error

--- client/src/constants/NavbarItems.js
+++ client/src/constants/NavbarItems.js
@@ -1,6 +1,6 @@
 export const userLinks = [
   { key: 1, name: 'Home', href: '/' },
-  { key: 2, name: 'About', href: '/about' },
+  { key: 2, name: 'Contact', href: '/contact-us' },
 ];
 
 export const busOperatorLinks = [
@@ -8,12 +8,6 @@ export const busOperatorLinks = [
   { key: 2, name: 'Bus Management', href: '/bo/bus' },
   { key: 3, name: 'Sales Revenue', href: '/bo/sales-revenue' },
   { key: 4, name: 'Rates & Review', href: '/bo/rates-and-reviews' },
-];
-
-export const memberLinks = [{ key: 2, name: 'Booking', href: '/trips' }];
-  { key: 1, name: 'Dashboard', href: '/bo/dashboard' },
-  { key: 2, name: 'Bus Management', href: '/bo/bus' },
-  { key: 3, name: 'Rates & Review', href: '/bo/rates-and-reviews' },
 ];
 
 export const memberLinks = [{ key: 2, name: 'Booking', href: '/trips' }];
