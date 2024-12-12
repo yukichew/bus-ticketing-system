0c2b46e010510bf47ef71ccad0216a83bfb2fc87
Yuki Chew <115348525+yukichew@users.noreply.github.com>
2024-12-12 23:32:26 +0800

    Solve auth bugs (#137)

--- client/src/utils/s3.js
+++ client/src/utils/s3.js
@@ -1,5 +1,5 @@
-export const accessKeyId = '';
-export const secretAccessKey = '';
-export const sessionToken = '';
-export const region = '';
-export const bucket = '';
+export const accessKeyId = process.env.REACT_APP_AWS_ACCESS_KEY_ID;
+export const secretAccessKey = process.env.REACT_APP_AWS_SECRET_ACCESS_KEY;
+export const sessionToken = process.env.REACT_APP_AWS_SESSION_TOKEN;
+export const region = process.env.REACT_APP_AWS_REGION;
+export const bucket = process.env.REACT_APP_AWS_BUCKET_NAME;
